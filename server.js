ac1d1b689b288fbf641821b97a8b743a7a0d166e
Samuel Shih <samuels.shih@gmail.com>
2018-10-11 15:16:59 -0700

    Added new files

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-const express = require ('express');
+require('newrelic');
+const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 var compression = require('compression');
 
 var server = express();
-server.use(compression())
+server.use(compression());
 server.use(bodyParser.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, './'), { maxAge: '30 days' }));
 server.use(cors());
 
 // Albums & Player
-server.get('/artists/albums/:artistID', (req, res) => {
-  res.redirect('http://52.15.129.193' + req.url);
+server.get('/artists/:artistId/albums', (req, res) => {
+  console.log('REQUEST URL ', req.url);
+  res.redirect('http://13.57.38.215:3000' + req.url);
 });
 
-// Related Artists
-server.get('/artist/:id/relatedArtists', (req, res) => {
-  res.redirect('http://18.206.245.56' + req.url);
-});
+// // Related Artists
+// server.get('/artist/:id/relatedArtists', (req, res) => {
+//   res.redirect('http://18.206.245.56' + req.url);
+// });
 
 // Popular Songs
 server.get('/artist/:id', (req, res) => {
-  res.redirect('http://18.224.17.253' + req.url);
+  res.redirect('http://13.56.254.143' + req.url);
 });
 
-// Header
-server.get('/artists/:artistID', (req, res) => {
-   res.redirect('http://35.172.133.115' + req.url);
-});
+// // Header
+// server.get('/artists/:artistID', (req, res) => {
+//   res.redirect('http://35.172.133.115' + req.url);
+// });
 
 server.listen(3000, console.log('Listening on:', 3000));
