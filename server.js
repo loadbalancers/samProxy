89b7b900d14d8527b68cc759608592185679f598
Samuel Shih <samuels.shih@gmail.com>
2018-10-11 19:24:21 -0700

    Put services together

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 var compression = require('compression');
+var request = require('request');
 
 var server = express();
 server.use(compression());
@@ -14,23 +15,31 @@ server.use(cors());
 
 // Albums & Player
 server.get('/artists/:artistId/albums', (req, res) => {
-  console.log('REQUEST URL ', req.url);
-  res.redirect('http://13.57.38.215:3000' + req.url);
+  req.pipe(request('http://13.57.38.215:3000' + req.url)).pipe(res);
 });
 
-// // Related Artists
-// server.get('/artist/:id/relatedArtists', (req, res) => {
-//   res.redirect('http://18.206.245.56' + req.url);
-// });
+// Related Artists
+server.get('*/related-artists', (req, res) => {
+  res.redirect(
+    `http://34.239.128.16:3005/ra-module/${Math.floor(
+      Math.random() * 1000
+    )}/related-artists`
+  );
+});
 
 // Popular Songs
 server.get('/artist/:id', (req, res) => {
   res.redirect('http://13.56.254.143' + req.url);
 });
 
-// // Header
-// server.get('/artists/:artistID', (req, res) => {
-//   res.redirect('http://35.172.133.115' + req.url);
-// });
+// Header
+server.get('/artists/:artistID', (req, res) => {
+  res.redirect('http://54.193.126.17:3004' + req.url);
+});
+
+// Locations
+server.get('/locations', (req, res) => {
+  res.redirect('http://54.193.126.17:3004' + req.url);
+});
 
 server.listen(3000, console.log('Listening on:', 3000));
